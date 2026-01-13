d8122e863a3509a670930d202712857079ff55b6
agussfw17 <agusfw17@gmail.com>
2026-01-13 13:49:15 -0300

    a

--- src/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getClientes } = require("../apis/clientes/clientes");
-
-router.get("/", getClientes);
-
-module.exports = router;
